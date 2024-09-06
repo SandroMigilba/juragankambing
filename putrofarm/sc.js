ef8e693e5127b8cb3783755f87dcb81bb17fb88a
SandroMigilba <sandromigilba10@gmail.com>
2024-09-06 18:03:47 +0700

    Add files via upload

---
+++ putrofarm/sc.js
@@ -0,0 +1,6 @@
+// Ini adalah tempat untuk menambahkan JavaScript di masa mendatang
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Website siap!');
+    // Contoh: Tampilkan alert saat halaman dimuat
+    // alert('Selamat datang di Peternakan Sapi Kita!');
+});
